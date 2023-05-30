62eb7df2a744edfc669cdddf1568fe368604800b
InFluX <matinazami2003@gmail.com>
2023-05-30 10:58:55 +0330

    Add Chomsky Form and CYK Parsing algorithm

--- main.cpp
+++ main.cpp
@@ -3,207 +3,280 @@
 #include "Base.h"
 using namespace std;
 
-class NFA {
+class ContextFreeGrammer
+{
 public:
-    set<State*> Q;
-    set<char> alphabet;
-    State* q0;
-    set<State*> finals;
-    map<Transfer*, set<State*>> transferFunctions;
+    unordered_set<char> variables;
+    unordered_set<char> terminals;
+    char startVariable;
+    unordered_map<char, unordered_set<string>> productionRules;
+    ContextFreeGrammer *contextFreeGrammerForParsing;
+    ContextFreeGrammer *chomsky;
+    ContextFreeGrammer *greibach;
+    bool lambda;
 
-    NFA(const set<State *> &q, const set<char> &alphabet, State *q0, const set<State *> &finals,
-        const map<Transfer *, set<State *>> &transferFunctions) : Q(q), alphabet(alphabet), q0(q0), finals(finals),
-                                                                  transferFunctions(transferFunctions) {}
+    ContextFreeGrammer(unordered_set<char> variables, unordered_set<char> terminals, char startVariable,
+                       unordered_map<char, unordered_set<string>> productionRules)
+    {
+        this->variables = variables;
+        this->terminals = terminals;
+        this->startVariable = startVariable;
+        this->productionRules = productionRules;
+    }
 
-    DFA* NFAToDFA(NFA* nfa)
+    ContextFreeGrammer(unordered_set<char> variables, unordered_set<char> terminals, char startVariable)
+    {
+        this->variables = variables;
+        this->terminals = terminals;
+        this->startVariable = startVariable;
+    }
+
+    bool parsingCYK(string s)
+    {
+        if (chomsky == nullptr)
+            makeChomsky();
+
+        unordered_map<string, unordered_set<char>> cyk;
+        for (int i = 0; i < s.length(); i++)
+        {
+            for (pair<char, unordered_set<string>> p : chomsky->productionRules)
+            {
+                if (p.second.find(string(1, s[i])) != p.second.end())
+                {
+                    cyk[string(1, s[i])].insert(p.first);
+                }
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++)
+            for (int j = i; j < s.length(); j++)
+            {
+                string m = s.substr(j - i, i + 1);
+                for (int k = 1; k < i + 1; k++)
+                {
+                    string u = m.substr(0, k);
+                    string v = m.substr(k);
+                    for (char c1 : cyk[u])
+                        for (char c2 : cyk[v])
+                            for (pair<char, unordered_set<string>> p : chomsky->productionRules)
+                                if (p.second.find(string(1, c1) + string(1, c2)) != p.second.end())
+                                    cyk[m].insert(p.first);
+                }
+            }
+
+        if (cyk[s].find(chomsky->startVariable) != cyk[s].end())
+            return true;
+        else
+            return false;
+    }
+
+    bool parsingRegular(string s)
     {
 
     }
-};
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-/**
-Author : Ishita Sharma
-My practical file which contains examples and theory - Link (https://docs.google.com/document/d/1J_GR_ahWRLsUX14rKKI9x8d4u2FqLTxv/edit?usp=sharing&ouid=107299008404639735755&rtpof=true&sd=true)
-**/
-void print(vector<vector<vector<int> > > table){
-    cout<<"  STATE/INPUT  |";
-    char a='a';
-    for(int i=0;i<table[0].size()-1;i++){
-        cout<<"   "<<a++<<"   |";
-    }
-    cout<<"   ^   "<<endl<<endl;
-    for(int i=0;i<table.size();i++){
-        cout<<"       "<<i<<"      ";
-        for(int j=0;j<table[i].size();j++){
-            cout<<" | ";
-            for(int k=0;k<table[i][j].size();k++){
-                cout<<table[i][j][k]<<" ";
 
-            }
-        }
-        cout<<endl;
+private:
+    char findVariable()
+    {
+        for (int i = 'A'; i <= 'Z'; i++)
+            if (chomsky->variables.find(char(i)) == chomsky->variables.end())
+                return char(i);
     }
-}
 
-void printdfa(vector<vector<int> > states, vector<vector<vector<int> > > dfa){
-    cout<<"  STATE/INPUT  ";
-    char a='a';
-    for(int i=0;i<dfa[0].size();i++){
-        cout<<"|   "<<a++<<"   ";
-    }
-    cout<<endl;
-    for(int i=0;i<states.size();i++){
-        cout<<"{ ";
-        for(int h=0;h<states[i].size();h++)
-            cout<<states[i][h]<<" ";
-        if(states[i].empty()){
-            cout<<"^ ";
-        }
-        cout<<"} ";
-        for(int j=0;j<dfa[i].size();j++){
-            cout<<" | ";
-            for(int k=0;k<dfa[i][j].size();k++){
-                cout<<dfa[i][j][k]<<" ";
-
-            }
-            if(dfa[i][j].empty()){
-                cout<<"^ ";
-            }
-        }
-        cout<<endl;
-    }
-}
-vector<int> closure(int s,vector<vector<vector<int> > > v){
-    vector<int> t;
-    queue<int> q;
-    t.push_back(s);
-    int a=v[s][v[s].size()-1].size();
-    for(int i=0;i<a;i++){
-        t.push_back(v[s][v[s].size()-1][i]);
-        //cout<<"t[i]"<<t[i]<<endl;
-        q.push(t[i]);
-    }
-    while(!q.empty()){
-        int f=q.front();
-        q.pop();
-        if(!v[f][v[f].size()-1].empty()){
-            int u=v[f][v[f].size()-1].size();
-            for(int i=0;i<u;i++){
-                int y=v[f][v[f].size()-1][i];
-                if(find(t.begin(),t.end(),y)==t.end()){
-                    //cout<<"y"<<y<<endl;
-                    t.push_back(y);
-                    q.push(y);
+    void removeLambda()
+    {
+        ContextFreeGrammer *cFG = this->contextFreeGrammerForParsing;
+        cFG->lambda = productionRules[startVariable].find("$") != cFG->productionRules[startVariable].end();
+        for (char variable : cFG->variables)
+            if (cFG->productionRules[variable].find("$") != cFG->productionRules[variable].end())
+                for (pair<char, unordered_set<string>> prRule : cFG->productionRules)
+                {
+                    unordered_set<string> newRule;
+                    for (string s : prRule.second)
+                    {
+                        int pos = 0;
+                        while (s.find(variable, pos) != -1)
+                        {
+                            string s1 = s.substr(0, s.find(variable, pos));
+                            string s2 = s.substr(s.find(variable, pos) + 1);
+                            pos = s.find(variable, pos) + 1;
+                            string fin = s1 + s2;
+                            if (fin.empty() || (fin.length() == 1 && fin[0] == prRule.first))
+                                continue;
+                            newRule.insert(fin);
+                        }
+                        if (s == "$" || (s.length() == 1 && s[0] == prRule.first))
+                            continue;
+                        newRule.insert(s);
+                    }
+                    cFG->productionRules[prRule.first] = newRule;
                 }
-            }
-        }
     }
-    return t;
-}
-int main(){
-    int n,alpha;
-    cout<<"************************* NFA to DFA *************************"<<endl<<endl;
-    cout<<"Enter total number of states in NFA : ";
-    cin>>n;
-    cout<<"Enter number of elements in alphabet : ";
-    cin>>alpha;
-    vector<vector<vector<int> > > table;
-    for(int i=0;i<n;i++){
-        cout<<"For state "<<i<<endl;
-        vector< vector< int > > v;
-        char a='a';
-        int y,yn;
-        for(int j=0;j<alpha;j++){
-            vector<int> t;
-            cout<<"Enter no. of output states for input "<<a++<<" : ";
-            cin>>yn;
-            cout<<"Enter output states :"<<endl;
-            for(int k=0;k<yn;k++){
-                cin>>y;
-                t.push_back(y);
-            }
-            v.push_back(t);
-        }
-        vector<int> t;
-        cout<<"Enter no. of output states for input ^ : ";
-        cin>>yn;
-        cout<<"Enter output states :"<<endl;
-        for(int k=0;k<yn;k++){
-            cin>>y;
-            t.push_back(y);
-        }
-        v.push_back(t);
-        table.push_back(v);
-    }
-    cout<<"***** TRANSITION TABLE OF NFA *****"<<endl;
-    print(table);
-    cout<<endl<<"***** TRANSITION TABLE OF DFA *****"<<endl;
-    vector<vector<vector<int> > > dfa;
-    vector<vector<int> > states;
-    states.push_back(closure(0,table));
-    queue<vector<int> > q;
-    q.push(states[0]);
-    while(!q.empty()){
-        vector<int> f=q.front();
-        q.pop();
-        vector<vector<int> > v;
-        for(int i=0;i<alpha;i++){
-            vector<int> t;
-            set<int> s;
-            for(int j=0;j<f.size();j++){
 
-                for(int k=0;k<table[f[j]][i].size();k++){
-                    vector<int> cl= closure(table[f[j]][i][k],table);
-                    for(int h=0;h<cl.size();h++){
-                        if(s.find(cl[h])==s.end())
-                            s.insert(cl[h]);
+    void removeNotAvailable()
+    {
+        unordered_set<char> vTemp;
+
+        queue<char> q;
+        q.push(this->startVariable);
+
+        while (!q.empty())
+        {
+            char u = q.front();
+            q.pop();
+            vTemp.insert(u);
+
+            for (string s : productionRules[u])
+                for (char c : s)
+                    if (vTemp.find(c) == vTemp.end() && variables.find(c) != variables.end())
+                        q.push(c);
+        }
+
+        contextFreeGrammerForParsing = new ContextFreeGrammer(vTemp, terminals, startVariable);
+
+        for (char v : variables)
+            if (vTemp.find(v) != vTemp.end())
+                contextFreeGrammerForParsing->productionRules[v] = productionRules[v];
+    }
+
+    void removeUnitRules()
+    {
+        ContextFreeGrammer *cFG = this->contextFreeGrammerForParsing;
+        for (char v : cFG->variables)
+            for (string s : cFG->productionRules[v])
+                if (s.size() == 1 && cFG->variables.find(s[0]) != cFG->variables.end())
+                    for (pair<char, unordered_set<string>> prRule : cFG->productionRules)
+                    {
+                        unordered_set<string> newRule;
+                        for (string su : prRule.second)
+                        {
+                            int pos = 0;
+                            while (su.find(v, pos) != -1)
+                            {
+                                string s1 = su.substr(0, su.find(v, pos));
+                                string s2 = su.substr(su.find(v, pos) + 1);
+                                pos = su.find(v, pos) + 1;
+                                string fin = s1 + s + s2;
+                                newRule.insert(fin);
+                            }
+                            newRule.insert(su);
+                        }
+                        cFG->productionRules[prRule.first] = newRule;
+                    }
+
+        for (char v : cFG->variables)
+        {
+            unordered_set<string> tmp;
+            for (string s : cFG->productionRules[v])
+                if (s.size() == 1 && cFG->variables.find(s[0]) != cFG->variables.end())
+                    tmp.insert(s);
+
+            for (string s : tmp)
+                cFG->productionRules[v].erase(s);
+        }
+    }
+
+    void makeContextFreeGrammerForParsing()
+    {
+        removeNotAvailable();
+        removeLambda();
+        removeUnitRules();
+    }
+
+    void makeChomsky()
+    {
+        makeContextFreeGrammerForParsing();
+        this->chomsky = new ContextFreeGrammer(variables, terminals, startVariable);
+        this->chomsky->lambda = this->lambda;
+
+        int tIdx = 0, vIdx = 0;
+        unordered_map<char, char> TtoV;
+        for (char t : terminals)
+        {
+            char u = findVariable();
+            variables.insert(u);
+            TtoV[t] = u;
+            this->chomsky->productionRules[u].insert(string(1, t));
+            chomsky->variables.insert(u);
+        }
+
+        ContextFreeGrammer *CFP = this->contextFreeGrammerForParsing;
+        for (pair<char, unordered_set<string>> v : CFP->productionRules)
+        {
+            for (string s : v.second)
+            {
+                for (int i = 0; s.length() > 1 && i < s.length(); i++)
+                {
+                    if (CFP->terminals.find(s[i]) != CFP->terminals.end())
+                        s[i] = TtoV[s[i]];
+                }
+
+                char lst;
+                for (int i = 0; s.length() > 2 && i < s.length() - 1; i++)
+                {
+                    if (i == 0)
+                    {
+                        lst = findVariable();
+                        chomsky->productionRules[v.first].insert(string(1, s[i]) + lst);
+                    }
+                    else if (i == s.length() - 2)
+                    {
+                        chomsky->productionRules[lst].insert(string(1, s[s.length() - 2]) + string(1, s[s.length() - 1]));
+                        chomsky->variables.insert(lst);
+                    }
+                    else
+                    {
+                        chomsky->variables.insert(lst);
+                        char nLst = findVariable();
+                        chomsky->productionRules[lst].insert(string(1, s[i]) + string(1, nLst));
+                        lst = nLst;
                     }
                 }
-            }
-            for(set<int >::iterator u=s.begin(); u!=s.end();u++)
-                t.push_back(*u);
-            v.push_back(t);
-            if(find(states.begin(),states.end(),t)==states.end())
-            {
-                states.push_back(t);
-                q.push(t);
+
+                if (s.length() <= 2)
+                    chomsky->productionRules[v.first].insert(s);
             }
         }
-        dfa.push_back(v);
     }
-    printdfa(states,dfa);
-}
+};
 
-//int main()
-//{
-////    State *s1 = new State("1", false);
-////    State *s2 = new State("2", false);
-////    State *s3 = new State("3", true);
-////    set<State*> Q = {s1, s2, s3};
-////
-////    set<char> alph = {'a', 'b'};
-////    State* q0 = s1;
-////    set<State*> finals = {s3};
-////
-////    map<Transfer*, State*> transferFunctions;
-////    Transfer* transfer1 = new Transfer(s1, 'a');
-////    Transfer* transfer2 = new Transfer(s1, 'b');
-////    Transfer* transfer3 = new Transfer(s2, 'a');
-////    Transfer* transfer4 = new Transfer(s2, 'b');
-////    Transfer* transfer5 = new Transfer(s3, 'a');
-////    Transfer* transfer6 = new Transfer(s3, 'b');
-////
-////    transferFunctions[transfer1] = s3;
-////    transferFunctions[transfer2] = s2;
-////    transferFunctions[transfer3] = s2;
-////    transferFunctions[transfer4] = s2;
-////    transferFunctions[transfer5] = s1;
-////    transferFunctions[transfer6] = s3;
-////
-////    DFA *dfa = new DFA(Q, alph, q0, finals, transferFunctions);
-////    cout << dfa->acceptance("abbaabbbaa");
-//
-//    return 0;
-//}
+int main()
+{
+    /*State *s1 = new State("1", false);
+    State *s2 = new State("2", false);
+    State *s3 = new State("3", true);
+    set<State *> Q = {s1, s2, s3};
+
+    set<char> alph = {'a', 'b'};
+    State *q0 = s1;
+    set<State *> finals = {s3};
+
+    map<Transfer *, set<State *>> transferFunctions;
+    Transfer *transfer1 = new Transfer(s1, 'a');
+    Transfer *transfer2 = new Transfer(s1, '$');
+    Transfer *transfer3 = new Transfer(s2, 'b');
+    Transfer *transfer5 = new Transfer(s3, 'a');
+    Transfer *transfer6 = new Transfer(s3, 'b');
+    Transfer *transfer4 = new Transfer(s3, '$');
+
+    transferFunctions[transfer1] = {s2, s3};
+    transferFunctions[transfer2] = {s2};
+    transferFunctions[transfer3] = {s2};
+    transferFunctions[transfer4] = {s3};
+    transferFunctions[transfer5] = {s3};
+    transferFunctions[transfer6] = {s3};
+
+    NFA *dfa = new NFA(Q, alph, q0, finals, transferFunctions);
+    cout << dfa->acceptance("$$$a", 0, q0);*/
+    
+    unordered_set<char> variables = {'S', 'A', 'B'};
+    unordered_set<char> terminals = {'a', 'b'};
+    char startVariable = 'S';
+    unordered_map<char, unordered_set<string>> productionRules;
+    productionRules['S'] = {"AB"};
+    productionRules['A'] = {"a", "BB"};
+    productionRules['B'] = {"AB", "b"};
+    ContextFreeGrammer *cfg = new ContextFreeGrammer(variables, terminals, startVariable, productionRules);
+    cout << cfg->parsing("aabb");
+    return 0;
+}
